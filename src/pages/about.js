7b546ae3668170f6de7bba4b88f1d2a7a7f78f65
Benoit Durand <benoit.durand.indep@gmail.com>
2022-11-21 15:17:00 +0700

    add layout and blog page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,17 +1,18 @@
 // Step 1: Import React
+import Layout from '../components/layout'
 import * as React from 'react'
+import Seo from '../components/seo'
 
 // Step 2: Define your component
 const AboutPage = () => {
   return (
-    <main>
-      <h1>About Me</h1>
+    <Layout pageTitle="About Me">
       <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
-    </main>
+    </Layout>
   )
 }
 
-export const Head = () => <title>About Me</title>
+export const Head = () => <Seo title="About me" />
 
 // Step 3: Export your component
 export default AboutPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,22 @@
 // Step 1: Import React
+import Layout  from '../components/layout'
 import * as React from 'react'
+import { StaticImage} from 'gatsby-plugin-image'
+import Seo from '../components/seo'
 
 // Step 2: Define your component
 const IndexPage = () => {
   return (
-    <main>
-      <h1>Welcome to my Gatsby site!</h1>
+    <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
-    </main>
+      <StaticImage alt="A japanese thing" src="https://origamijapan.net/wp-content/uploads/2017/10/tomoe-mon_2000x1400.jpg"/>
+      <StaticImage alt="A japanese symbol" src="../images/Koi.jpg"/>
+    </Layout>
   )
 }
 
 // You'll learn about this in the next task, just copy it for now
-export const Head = () => <title>Home Page</title>
+export const Head = () => <Seo title="Home Page" />
 
 // Step 3: Export your component
 export default IndexPage
