22fc6fe94112b80f04649998baee9c387683b23c
Benoit Durand <benoit.durand.indep@gmail.com>
2022-11-23 19:57:22 +0700

    add mdx posts incompatibility issues with react 18

--- src/components/layout.js
+++ src/components/layout.js
@@ -19,7 +19,7 @@ const Layout = ({ pageTitle, children }) => {
                 <ul className={navLinks}>
                     <li className={navLinkItem}><Link to="/">Home</Link></li>
                     <li className={navLinkItem}><Link to="/about" className={navLinkText}>About</Link></li>
-                    <li className={navLinkItem}><Link to="/blog" className={navLinkText}>
+                    <li className={navLinkItem}><Link to="/blog/blog" className={navLinkText}>
                         Blog
                     </Link>
                     </li>
